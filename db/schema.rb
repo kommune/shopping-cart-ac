6e6a921ef62f6f9d0d90268ff1679fa39e36d36a
kommune <kommune.000@gmail.com>
2018-03-06 16:04:37 +0800

    Permit params

--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,9 @@ ActiveRecord::Schema.define(version: 20180305093716) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.integer "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "categories_products", force: :cascade do |t|
