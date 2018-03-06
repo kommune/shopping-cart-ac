e826ff8ed07e08580f1dff2179e21e8918f804fd
kommune <kommune.000@gmail.com>
2018-03-06 14:25:40 +0800

    Generate admin/categories and admin/products controller

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,9 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
-  validates :username, presence: true
+  validates_presence_of :email
+  validates_presence_of :encrypted_password
   
 end
