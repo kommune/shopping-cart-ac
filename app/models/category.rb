adb67c77eca1f21aa394b2839733c8d36fc89e87
Liyan <liyan@liyanchua.com>
2018-03-06 14:20:57 +0800

    create categories controller and views

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ApplicationRecord
   has_many :products, through: :categories_products
   has_many :categories_products, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
 end
---
+++ spec/controllers/catergories_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CatergoriesController, type: :controller do
+
+end
