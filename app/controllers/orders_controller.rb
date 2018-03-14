18066ce357e9c24974e53454f7ae29f35fd9785d
Liyan <liyan@liyanchua.com>
2018-03-14 15:44:58 +0800

    Update order view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @order = current_user.orders.find(params[:id])
+    @orders = current_user.orders.all
   end
 
   def show
