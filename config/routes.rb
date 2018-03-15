3d82598bceeb3053d2e193b12cf43c2e958fddab
kommune <kommune.000@gmail.com>
2018-03-15 17:31:06 +0800

    Fix collection buttons and cart issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations", omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
   
